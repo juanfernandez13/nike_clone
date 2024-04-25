43873d2c21aee93dae1de100a03e0e87a633357d
Juan <juanp1310@outlook.com>
2024-04-25 14:48:30 -0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Button from "./Button";
+import Nav from "./Nav";
+import PopularProductsCard from "./PopularProductsCard";
+import ShoeCard from "./ShoeCard";
+import ReviewCard from "./ReviewCard"
+
+export {
+  Button,
+  Nav,
+  PopularProductsCard,
+  ShoeCard,
+  ReviewCard,
+
+}
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+
+export {
+  Hero,
+  Footer,
+  Services,
+  Subscribe,
+  SuperQuality,
+  SpecialOffer,
+  CustomerReviews,
+  PopularProducts
+}
